7131f51912d16d6c9d10027e138903632fcdd814
vincent rhino <vincent.rhino@smile.fr>
2014-08-27 14:55:48 +0200

    pagination foctory + pagination params

--- src/Virhi/Bundle/PaginationBundle/Factory/FixedPaginationFactory.php
+++ src/Virhi/Bundle/PaginationBundle/Factory/FixedPaginationFactory.php
@@ -9,24 +9,22 @@
 namespace Virhi\Bundle\PaginationBundle\Factory;
 
 use Virhi\Bundle\PaginationBundle\Pagination\FixedPagination;
+use Virhi\Bundle\PaginationBundle\Pagination\Params\PaginationParams;
 use Virhi\Bundle\PaginationBundle\Pagination\Params\RangePaginationParams;
 
 
-class FixedPaginationFactory
+class FixedPaginationFactory extends PaginationFactory
 {
-
-    protected $router;
-
-    function __construct($router)
-    {
-        $this->router = $router;
-    }
 
     /**
      * @return FixedPagination
      */
-    public function getPagination(RangePaginationParams $params)
+    public function getPagination(PaginationParams $params)
     {
+        if (!$params instanceof RangePaginationParams) {
+            throw new \RuntimeException('Wrong Pagination Params, need RangePaginationParams');
+        }
+
         $pagination = new FixedPagination(
             $this->router,
             $params->getRoute(),
---
+++ src/Virhi/Bundle/PaginationBundle/Factory/PaginationFactory.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: virhi
+ * Date: 27/08/2014
+ * Time: 14:49
+ */
+
+namespace Virhi\Bundle\PaginationBundle\Factory;
+
+use Virhi\Bundle\PaginationBundle\Pagination\Pagination;
+use Virhi\Bundle\PaginationBundle\Pagination\Params\PaginationParams;
+
+class PaginationFactory
+{
+    protected $router;
+
+    function __construct($router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * @return Pagination
+     */
+    public function getPagination(PaginationParams $params)
+    {
+        $pagination = new Pagination(
+            $this->router,
+            $params->getRoute(),
+            $params->getRouteParam(),
+            $params->getLimit(),
+            $params->getNbElement(),
+            $params->getOffset()
+        );
+        return $pagination;
+    }
+} 
